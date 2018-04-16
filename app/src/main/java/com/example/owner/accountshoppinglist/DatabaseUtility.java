88e87c15e6c9536846cb7432b061a14782e5cca0
caihua2400 <caihua2600@gmail.com>
2018-04-16 22:10:48 +1000

    file complete and DatabaseUtility completed

--- app/src/main/java/com/example/owner/accountshoppinglist/DatabaseUtility.java
+++ app/src/main/java/com/example/owner/accountshoppinglist/DatabaseUtility.java
@@ -1,5 +1,11 @@
 package com.example.owner.accountshoppinglist;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
 /**
  * Created by Owner on 16/04/2018.
  */
@@ -33,4 +39,78 @@ public class DatabaseUtility {
             + KEY_ITEM_PHOTO_PATH+ " string not null, "
             + KEY_TAG+" string not null"
             +");";
+
+    public static void insert(SQLiteDatabase db, ShoppingItem s){
+        ContentValues values=new ContentValues();
+        values.put(KEY_NAME,s.getName());
+        values.put(KEY_PRICE,s.getPrice());
+        values.put(KEY_QUANTITY,s.getQuantity());
+        values.put(KEY_ITEM_PHOTO_PATH,s.getPath());
+        values.put(KEY_TAG,s.getTag());
+        db.insert(TABLE_NAME,null,values);
+    }
+    public static void insert_bought(SQLiteDatabase db,ShoppingItem s){
+        ContentValues values=new ContentValues();
+        values.put(KEY_NAME,s.getName());
+        values.put(KEY_PRICE,s.getPrice());
+        values.put(KEY_QUANTITY,s.getQuantity());
+        values.put(KEY_ITEM_PHOTO_PATH,s.getPath());
+        values.put(KEY_TAG,s.getTag());
+        db.insert(TABLE_NAME_BOUGHT,null,values);
+    }
+    public static void update(SQLiteDatabase db,ShoppingItem s){
+        ContentValues values=new ContentValues();
+        values.put(KEY_NAME,s.getName());
+        values.put(KEY_PRICE,s.getPrice());
+        values.put(KEY_QUANTITY,s.getQuantity());
+        values.put(KEY_ITEM_PHOTO_PATH,s.getPath());
+        values.put(KEY_TAG,s.getTag());
+        db.update(TABLE_NAME,values,KEY_SHOPPINGITEM_ID+"= ?",new String[]{""+s.getId()});
+
+    }
+    public static void delete(SQLiteDatabase db, ShoppingItem s){
+        db.delete(TABLE_NAME,KEY_SHOPPINGITEM_ID+"= ?",new String[]{""+s.getId()});
+
+    }
+    public static ArrayList<ShoppingItem> selectAll(SQLiteDatabase db){
+        ArrayList<ShoppingItem> results=new ArrayList<ShoppingItem>();
+        Cursor c= db.query(TABLE_NAME,null,null,null,null,null,null);
+        if(c!=null){
+            c.moveToNext();
+            while(!c.isAfterLast()){
+                ShoppingItem p=createFromCursor(c);
+                results.add(p);
+                c.moveToNext();
+            }
+        }
+        return results;
+    }
+    public static ArrayList<ShoppingItem> selectAll_bought(SQLiteDatabase db){
+        ArrayList<ShoppingItem> results=new ArrayList<ShoppingItem>();
+        Cursor c= db.query(TABLE_NAME_BOUGHT,null,null,null,null,null,null);
+        if(c!=null){
+            c.moveToNext();
+            while(!c.isAfterLast()){
+                ShoppingItem p=createFromCursor(c);
+                results.add(p);
+                c.moveToNext();
+            }
+        }
+        return results;
+    }
+    private static ShoppingItem createFromCursor(Cursor c) {
+        if(c==null || c.isAfterLast() || c.isBeforeFirst()){
+            return null;
+        }else{
+            ShoppingItem p=new ShoppingItem();
+            p.setId(c.getInt(c.getColumnIndex(KEY_SHOPPINGITEM_ID)));
+            p.setName(c.getString(c.getColumnIndex(KEY_NAME)));
+            p.setPrice(c.getInt(c.getColumnIndex(KEY_PRICE)));
+            p.setQuantity(c.getInt(c.getColumnIndex(KEY_QUANTITY)));
+            p.setPath(c.getString(c.getColumnIndex(KEY_ITEM_PHOTO_PATH)));
+            p.setTag(c.getString(c.getColumnIndex(KEY_TAG)));
+            return p;
+
+        }
+    }
 }
---
+++ app/src/main/java/com/example/owner/accountshoppinglist/MainListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.owner.accountshoppinglist;
+
+/**
+ * Created by Owner on 16/04/2018.
+ */
+
+public class MainListAdapter {
+}
